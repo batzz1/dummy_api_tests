b0299cf8d3bfffd511d4ea3f6d70e532bb818fe5
Gaurav Sharma <grvsharma@hotmail.com>
2020-03-25 17:32:18 +0530

    Added Lombok dependency

---
+++ src/main/java/entities/employee/response/GetEmployeeResponse.java
@@ -0,0 +1,9 @@
+package entities.employee.response;
+
+import entities.BaseResponse;
+
+
+public class GetEmployeeResponse extends BaseResponse {
+    private Data data;
+    private String status;
+}
